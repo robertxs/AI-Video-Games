e271da6be0966a5a58f4a69d880b812507eca20f
robertxs <robertorrt7@gmail.com>
2018-11-06 21:00:15 -0400

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private int speed = 100;
+
+    private void Update()
+    {
+
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        transform.position += Movement * speed * Time.deltaTime;
+
+    }
+}
